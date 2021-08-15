c5874d7402054e4f54ce5f6e1db622f6e40809b0
Stephan516 <stephanwang8487@gmail.com>
2021-08-15 15:58:11 +0800

    TestCast.java

---
+++ TestCast.java
@@ -0,0 +1,20 @@
+package module01_06;
+/*
+ * 此範例為測為型別晉升與型別轉換
+ */
+public class TestCast {
+
+private string dog;
+private string cat;
+
+
+	public static void main(String[] args) {
+		int i = 1;
+		double d = 11.1;
+		double sum1 = i + d;
+		int sum2 = (int)(i + d);
+		System.out.println(sum1);
+		System.out.println(sum2);
+	}
+
+}
